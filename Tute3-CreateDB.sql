08c78fb8a9e3002171abe53a22e6154f403cc693
lenadahlin <lenakarind@hotmail.com>
2023-02-28 12:28:27 +1100

    create tables done

--- Tute3-CreateDB.sql
+++ Tute3-CreateDB.sql
@@ -1,20 +1,42 @@
+DROP TABLE IF EXISTS Student --
+DROP TABLE IF EXISTS Course --
+DROP TABLE IF EXISTS Semester --
+DROP TABLE IF EXISTS Register --
+
 CREATE TABLE Student(
     stdNo CHAR(5) PRIMARY KEY,
     login CHAR(6),
     lastName VARCHAR(25),
     givenNames VARCHAR(50),
     programCode CHAR(4)
-)
-
-CREATE TABLE Course (
+) CREATE TABLE Course (
     courseID CHAR(8) PRIMARY KEY,
     cName VARCHAR(25),
-    credits INT(3) CHECK (credits>=0 AND credits <=100)
-    --assumed knowledge FK
-)
-
-CREATE TABLE Semester (
+    credits INT CHECK (
+        credits >= 0
+        AND credits <= 100
+    ) DEFAULT '20',
+    assumedKnowledge CHAR(8),
+    FOREIGN KEY (assumedKnowledge) REFERENCES Course(courseID) ON UPDATE NO ACTION ON DELETE NO ACTION
+) CREATE TABLE Semester (
     semesterID INT PRIMARY KEY CHECK (semesterID > 0),
-    semester INT CHECK (semester >= 1 AND semester <= 4),
-    year INT CHECK (year >= 2000 AND year <= 9999)
-)
+    semester INT CHECK (
+        semester >= 1
+        AND semester <= 4
+    ),
+    year INT CHECK (
+        year >= 2000
+        AND year <= 9999
+    )
+) CREATE TABLE Register (
+    stdNo CHAR(5), 
+    courseID CHAR(8),
+    semesterID INT,
+    grade CHAR(2),
+    mark DECIMAL(5,2) CHECK (mark >= 0 AND mark <= 100) DEFAULT '0.0',
+    PRIMARY KEY(stdNo, courseID, semesterID),
+    FOREIGN KEY (stdNo) REFERENCES Student(stdNo) ON UPDATE CASCADE ON DELETE NO ACTION,
+    FOREIGN KEY (courseID) REFERENCES Course(courseID) ON UPDATE CASCADE ON DELETE NO ACTION,
+    FOREIGN KEY (semesterID) REFERENCES Semester(semesterID) ON UPDATE CASCADE ON DELETE NO ACTION
+    )
+
